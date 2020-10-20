313f08ecb0c0a41ccc407ceb4f17dd42621eabb3
陈泽南 <chenzenan@jtkjbike.com>
2020-10-20 18:53:16 +0800

    日志打印参数优化

--- blog-web/src/main/java/com/czndata/blog/web/aop/WebLogAspect.java
+++ blog-web/src/main/java/com/czndata/blog/web/aop/WebLogAspect.java
@@ -5,12 +5,15 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 @Slf4j
 @Aspect
@@ -24,6 +27,8 @@ public class WebLogAspect {
     public void doBefore(JoinPoint joinPoint){
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
 
         String url = request.getRequestURI();
         String ip = request.getRemoteAddr();
@@ -37,7 +42,19 @@ public class WebLogAspect {
             ip = real;
         }
 
-        log.info("Url:{} IP:{} args:{}", url, ip, joinPoint.getArgs());
+        // 请求的方法参数
+        Object[] args = joinPoint.getArgs();
+        // 请求的方法参数名称
+        LocalVariableTableParameterNameDiscoverer u = new LocalVariableTableParameterNameDiscoverer();
+        String[] paramNames = u.getParameterNames(method);
+
+        StringBuilder params = new StringBuilder();
+        if (args != null && paramNames != null) {
+            for (int i = 0; i < args.length; i++) {
+                params.append("  ").append(paramNames[i]).append(": ").append(args[i]);
+            }
+        }
+        log.info("Url:{} IP:{} args:{}", url, ip, params.toString());
     }
 
     public boolean checkIp(String ip){
